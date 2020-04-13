6188423afaf3d67cfbb585fe60a3b7a106e2b207
yourname <yourname@site.com>
2020-04-13 23:43:06 +0500

    Creating Solution and adding first classes

---
+++ Fight for The Life/Domain/Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fight_for_The_Life.Domain
+{
+    class Game
+    {
+    }
+
+    class Sperm
+    {
+        public Point Location { get; }
+        private readonly Core Core = new Core();
+        public bool HasCore => Location == Core.Location;
+    }
+
+    class Obstruction
+    {
+    }
+
+    class Blood : Obstruction
+    {
+
+    }
+
+    class OtherSperm : Obstruction
+    {
+        
+    }
+
+    class Core
+    {
+        public Point Location { get; }
+    }
+}
---
+++ Fight for The Life/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fight_for_The_Life
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
